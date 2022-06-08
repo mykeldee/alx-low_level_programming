605908b0168d3db0becdb1dfa0f04b8e8bc289b7
Michael Dankyira <kdankyira.education@gmail.com>
2022-06-07 18:15:42 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,14 @@
  */
 int main(void)
 {
-	_putchar('putchar');
+	int myChar[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(mychar[i]);
+		i++;
+	}
 	_putchar('\n');
 	return 0;
 }
